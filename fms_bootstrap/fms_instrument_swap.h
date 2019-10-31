45bdf590d364c4829a51397263f5d9a348198e91
celinedamogu <55509088+celinedamogu@users.noreply.github.com>
2019-10-31 17:18:42 -0400

    cz2573 hw8

--- fms_bootstrap/fms_instrument_swap.h
+++ fms_bootstrap/fms_instrument_swap.h
@@ -5,13 +5,56 @@
 // Time is measured in years. Frequency is the number of coupons per year.
 #pragma once
 #include "fms_instrument_sequence.h"
+#include"cassert"
+#include<list>
+
 
 namespace fms::instrument {
-	
-	//!!! Implement helper functions.
-	// auto make_time(....) -> (0, 1/freq,  ..., maturity)
-	// auto make_cash(....) -> (-1, c/freq, ..., 1 + c/freq)
+	template<class U = double, class C = double>
 
-	//!!! Implement the class interest_rate_swap in namespace fms::instrument.
+	//!!! Implement helper functions.
+		// auto make_time(....) -> (0, 1/freq,  ..., maturity)
+	auto make_time(U maturity, U frequency) {
+
+		fms::sequence::list<U> temp{(0)};
+
+		assert(maturity > 0);
+		assert(frequency > 0);
+
+		for (int i = 1; i < frequency * maturity; i++) {
+			temp.push_back(i / frequency);
+		}
+		temp.push_back(maturity);
+		return temp;
+	}
+	// auto make_cash(....) -> (-1, c/freq, ..., 1 + c/freq)
+	template<class U = double, class C = double>
+	auto make_cash(U maturity, U frequency, C coupon) {
+		fms::sequence::list<C> temp{(-1)};
+
+		assert(maturity > 0);
+		assert(frequency > 0);
+		assert(coupon > 0);
+
+		for (int i = 1; i < frequency * maturity; i++) {
+			temp.push_back(coupon / frequency);
+		}
+		temp.push_back(1 + coupon / frequency);
+		return temp;
+	}
+	template<class U = double, class C = double>
+	struct interest_rate_swap : public sequence<fms::sequence::list<U>, fms::sequence::list<C>> {
+		interest_rate_swap(U maturity, U frequency, C coupon)
+			: sequence<fms::sequence::list<U>, fms::sequence::list<C>>(
+				make_time(maturity, frequency), make_cash(maturity, frequency, coupon)
+				)
+		{ }
+		
+		
+	};
 
 }
+
+
+
+
--- xll_bootstrap/xll_instrument.cpp
+++ xll_bootstrap/xll_instrument.cpp
@@ -126,5 +126,73 @@ HANDLEX WINAPI xll_instrument_cd(double tenor, double rate)
 }
 
 //!!! Implement INSTRUMENT.FORWARD_RATE_AGREEMENT(effective, tenor, forward)
+AddIn xai_instrument_forward_rate_agreement(
+	Function(XLL_HANDLE, L"?xll_instrument_forward_rate_agreement", CATEGORY L".FORWARD_RATE_AGREEMENT")
+	.Arg(XLL_FP, L"effective", L"is the time of year where the calculation of the forward rate starts.")
+	.Arg(XLL_FP, L"tenor", L"is the time of maturity.")
+	.Arg(XLL_FP, L"forward", L"is the compounding forward rate.")
+	.Uncalced()
+	.Category(CATEGORY)
+	.FunctionHelp(L"Return a handle to an forward rate instrument.")
+	.Documentation(
+		L"Forward rate agreements have two cash flows: -1 at the effective date"
+		L"and 1 + forward*tenor at the expriation date = effective + tenor. "
+	)
+);
+HANDLEX WINAPI xll_instrument_forward_rate_agreement(double effective, double tenor, double forward)
+{
+#pragma XLLEXPORT
+	handlex result;
+
+	try {
+		auto fra = fms::instrument::forward_rate_agreement(effective, tenor, forward);
+		handle<xll::instrument<>> fra_(new instrument_impl(fra));
+
+		result = fra_.get();
+
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+
+		return 0; // #NUM!
+	}
+
+	return result;
+}
 
 //!!! Implement INSTRUMENT.INTEREST_RATE_SWAP(maturity, frequency, coupon)
+AddIn xai_instrument_INTEREST_RATE_SWAP(
+	Function(XLL_HANDLE, L"?xll_instrument_interest_rate_swap", CATEGORY L".INTEREST_RATE_SWAP")
+	.Arg(XLL_FP, L"maturity", L"is the time of maturity.")
+	.Arg(XLL_FP, L"frequency", L"is the number of coupons per year.")
+	.Arg(XLL_FP, L"coupon", L"is the cashflows occurs before maturity.")
+	.Uncalced()
+	.Category(CATEGORY)
+	.FunctionHelp(L"Return a handle to an interest rate instrument.")
+	.Documentation(
+		L" A swap is determined by (maturity, frequency, coupon). "
+		L"It has cashflows -1 at 0, coupon/frequency, at times i/frequency for i = 1, 2, ..., n - 1,"
+		L" and 1 + coupon/frequence at maturity = n/frequency."
+	)
+);
+HANDLEX WINAPI xll_instrument_interest_rate_swap(double maturity, double frequency, double coupon)
+{
+#pragma XLLEXPORT
+	handlex result;
+
+	try {
+
+		auto swap = fms::instrument::forward_rate_agreement(maturity, frequency, coupon);
+		handle<xll::instrument<>> swap_(new instrument_impl(swap));
+
+		result = swap_.get();
+
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+
+		return 0; // #NUM!
+	}
+
+	return result;
+}
