c6a97c23a2264fbd5415998c4cc56768b1986c08
Fei Q <feiwqiu@gmail.com>
2019-10-31 19:11:13 -0400

    hw8final_fq2145

--- fms_bootstrap/fms_instrument_swap.h
+++ fms_bootstrap/fms_instrument_swap.h
@@ -8,31 +8,42 @@
 
 namespace fms::instrument {
 	
+	template<class U = double, class F = double, class C = double>
 	//Implement helper functions.
-	//	auto make_time(....) -> (0, 1/freq,  ..., maturity)
-	auto make_time(t);
-	t = n / frequency;
-	++n;
-	assert(maturity == t);
+	//auto make_time(....) -> (0, 1/freq,  ..., maturity)
+	auto make_time(const U& maturity, const F& frequency) {
+		assert(maturity > 0);
+		assert(frequency > 0);
+		fms::sequence::list<double> tm({ 0 });
+		int i;
+		for (i = 1; double(i) / frequency < maturity; i++) {
+			tm.push_back(double(i) / frequency);
+		}
+
 	// auto make_cash(....) -> (-1, c/freq, ..., 1 + c/freq)
-	auto make_cash(c) = c / frequency;
-	++n;
-	assert(maturity == make_cash());
+	auto make_cash(const U & maturity, const F & frequency, const C & coupon) {
+		assert(maturity > 0);
+		assert(frequency > 0);
+		fms::sequence::list<double> cs({ -1 });
+		int i;
+		for (i = 1; double(i) / frequency < maturity; i++) {
+			cs.push_back(coupon / frequency);
+		}
+		if (std::abs(double(i) / frequency - maturity) < std::numeric_limits<U>::epsilon()) {
+			cs.push_back(coupon / frequency + 1);
+		}
+		else {
+			cs.push_back(1);
+		}
+		return cs;
+	}
 
 	//Implement the class interest_rate_swap in namespace fms::instrument
-	
-
-	template<class U = double, class C = double>
-	struct interest_rate_swap : public sequence<fms::sequence::list<U>, fms::sequence::list<C>>{
-	interest_rate_swap(U maturity, U frequency, C coupon)
-			: sequence<fms::sequence::list<U>, fms::sequence::list<C>>, fms::sequence::list<F>>(
-				fms::sequence::list<U>({ 0, maturity }), fms::sequence::list<C>({ -1, 1 + coupon / frequency })
+	struct interest_rate_swap : public sequence<fms::sequence::list<U>, fms::sequence::list<C>> {
+	interest_rate_swap(U maturity, F frequency, C coupon)
+			: sequence<fms::sequence::list<U>, fms::sequence::list<C>>(
+				make_time(maturity, frequency), make_cash(maturity, frequency, coupon)
+				)
 		{ }
 	};
-
-	
-	
-	
-
-
 }
