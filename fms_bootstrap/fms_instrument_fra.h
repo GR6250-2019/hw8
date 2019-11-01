82013b24f14d02f582c0faf115a3cab54746a537
rl3107 <rl3107@columbia.edu>
2019-10-31 22:52:37 -0400

    Revert "hw 8  Ruiqing Liu rl3107"
    This reverts commit e7445e91d2f9b8f13a2da83475518e34fec525dd.

--- fms_bootstrap/fms_instrument_fra.h
+++ fms_bootstrap/fms_instrument_fra.h
@@ -2,13 +2,6 @@
 // Forward rate agreements have two cash flows: -1 at the effective date
 // and 1 + forward*tenor at the expriation date = effective + tenor.
 // The argument order for the constructor is (effective, tenor, forward)
-#pragma once//!!! Implement the class forward_rate_agreement in namespace fms::instrument.
-#include "fms_instrument_sequence.h"namespace fms::instrument { template<class U = double, class C = double>
-struct forward_rate_agreement : public sequence<fms::sequence::list<U>, fms::sequence::list<C>> {
-	forward_rate_agreement(U effective, U tenor, C forward)
-		: sequence<fms::sequence::list<U>, fms::sequence::list<C>>(
-			fms::sequence::list<U>({ effective, effective + tenor }), fms::sequence::list<C>({ -1, 1 + forward * tenor })
-			)
-	{ }
-};
-}
+#pragma once
+
+//!!! Implement the class forward_rate_agreement in namespace fms::instrument.
--- xll_bootstrap/xll_pwflat.cpp
+++ xll_bootstrap/xll_pwflat.cpp
@@ -1,15 +1,25 @@
-/ xll_pwflat.cpp - Excel add - in for piecewise flat forward curves.
+// xll_pwflat.cpp - Excel add-in for piecewise flat forward curves.
 #include "../fms_bootstrap/fms_pwflat.h"
-#include "xll_bootstrap.h"#ifdef CATEGORY
+#include "xll_bootstrap.h"
+
+#ifdef CATEGORY
 #undef CATEGORY
 #endif
-#define CATEGORY L"PWFLAT"using namespace xll;AddIn xai_pwflat(
+#define CATEGORY L"PWFLAT"
+
+using namespace xll;
+
+AddIn xai_pwflat(
 	Document(CATEGORY)
 	.Category(CATEGORY)
 	.Documentation(
 		L"Functions for piecewise flat forward curves. "
 	)
-); using forward = fms::pwflat::forward<fms::sequence::list<double>, fms::sequence::list<double>>; AddIn xai_pwflat_forward(
+);
+
+using forward = fms::pwflat::forward<fms::sequence::list<double>, fms::sequence::list<double>>;
+
+AddIn xai_pwflat_forward(
 	Function(XLL_HANDLE, L"?xll_pwflat_forward", CATEGORY L".FORWARD")
 	.Arg(XLL_FP, L"time", L"is an array of times. ")
 	.Arg(XLL_FP, L"cash", L"is an array of forwards. ")
@@ -21,16 +31,28 @@
 		L"The times must be increasing and positive. "
 	)
 );
-HANDLEX WINAPI xll_pwflat_forward(const _FP12 * pt, const _FP12 * pf)
+HANDLEX WINAPI xll_pwflat_forward(const _FP12* pt, const _FP12* pf)
 {
 #pragma XLLEXPORT
-	handlex result; try {
-		ensure(size(*pt) == size(*pf));  handle<forward> forward_(new forward(list(*pt), list(*pf)));  result = forward_.get();
+	handlex result;
+
+	try {
+		ensure(size(*pt) == size(*pf));
+
+		handle<forward> forward_(new forward(list(*pt), list(*pf)));
+
+		result = forward_.get();
 	}
 	catch (const std::exception & ex) {
-		XLL_ERROR(ex.what());  return 0; // #NUM!
-	} return result;
-}AddIn xai_pwflat_forward_value(
+		XLL_ERROR(ex.what());
+
+		return 0; // #NUM!
+	}
+
+	return result;
+}
+
+AddIn xai_pwflat_forward_value(
 	Function(XLL_FP, L"?xll_pwflat_forward_value", CATEGORY L".FORWARD.VALUE")
 	.Arg(XLL_HANDLE, L"forward", L"is a handle to a piecewise flat forward. ")
 	.Arg(XLL_FP, L"times", L"is an array of times at which to find the forward value. ")
@@ -40,63 +62,29 @@ HANDLEX WINAPI xll_pwflat_forward(const _FP12 * pt, const _FP12 * pf)
 		L"Return an array of forward values corresponding to times. "
 	)
 );
-_FP12* WINAPI xll_pwflat_forward_value(HANDLEX fwd, const _FP12 * pt)
+_FP12* WINAPI xll_pwflat_forward_value(HANDLEX fwd, const _FP12* pt)
 {
 #pragma XLLEXPORT
-	static xll::FP12 result; try {
-		handle<forward> fwd_(fwd);  result.resize(rows(*pt), columns(*pt));
+	static xll::FP12 result;
+
+	try {
+		handle<forward> fwd_(fwd);
+
+		result.resize(rows(*pt), columns(*pt));
 		for (int i = 0; i < size(*pt); ++i) {
 			result[i] = fwd_->value(pt->array[i]);
 		}
 	}
 	catch (const std::exception & ex) {
-		XLL_ERROR(ex.what());  return 0; // #NUM!
-	} return result.get();
-}//!!! Implement PWFLAT.FORWARD.SPOT
-AddIn xai_pwflat_forward_spot(
-	Function(XLL_FP, L"?xll_pwflat_forward_spot", CATEGORY L".FORWARD.SPOT")
-	.Arg(XLL_HANDLE, L"forward", L"is a handle to a piecewise flat forward. ")
-	.Arg(XLL_FP, L"times", L"is an array of times at which to find the forward spot. ")
-	.Category(CATEGORY)
-	.FunctionHelp(L"Return an array of forward spot corresponding to times. ")
-	.Documentation(
-		L"Return an array of forward spot corresponding to times. "
-	)
-);
-_FP12* WINAPI xll_pwflat_forward_spot(HANDLEX fwd, const _FP12 * pt)
-{
-#pragma XLLEXPORT
-	static xll::FP12 result; try {
-		handle<forward> fwd_(fwd);  result.resize(rows(*pt), columns(*pt));
-		for (int i = 0; i < size(*pt); ++i) {
-			result[i] = fwd_->value(pt->array[0]);
-		}
+		XLL_ERROR(ex.what());
+
+		return 0; // #NUM!
 	}
-	catch (const std::exception & ex) {
-		XLL_ERROR(ex.what());  return 0; // #NUM!
-	} return result.get();
+
+	return result.get();
 }
+
+//!!! Implement PWFLAT.FORWARD.SPOT
+
 //!!! Implement PWFLAT.FORWARD.DISCOUNT
-AddIn xai_pwflat_forward_discount(
-	Function(XLL_FP, L"?xll_pwflat_forward_discount", CATEGORY L".FORWARD.DISCOUNT")
-	.Arg(XLL_HANDLE, L"forward", L"is a handle to a piecewise flat forward. ")
-	.Arg(XLL_FP, L"times", L"is an array of times at which to find the forward discount. ")
-	.Category(CATEGORY)
-	.FunctionHelp(L"Return an array of forward discount corresponding to times. ")
-	.Documentation(
-		L"Return an array of forward discount. "
-	)
-);
-_FP12* WINAPI xll_pwflat_forward_discount(HANDLEX fwd, const _FP12 * pt)
-{
-#pragma XLLEXPORT
-	static xll::FP12 result; try {
-		handle<forward> fwd_(fwd);  result.resize(rows(*pt), columns(*pt));
-		for (int i = 0; i < size(*pt); ++i) {
-			result[i] = fwd_->value(pt->array[0]) / fwd_->value(pt->array[i]);
-		}
-	}
-	catch (const std::exception & ex) {
-		XLL_ERROR(ex.what());  return 0; // #NUM!
-	} return result.get();
-}
+
