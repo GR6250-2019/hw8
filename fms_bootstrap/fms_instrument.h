bead668ad9d81e599a0b471c94a2aa36a5a2ea17
rl3107 <rl3107@columbia.edu>
2019-10-31 23:27:34 -0400

    hw8 rl3107

--- fms_bootstrap/fms_instrument.h
+++ fms_bootstrap/fms_instrument.h
@@ -1,5 +1,6 @@
 // fms_instrument.h - Fixed income instrument
 #pragma once
 
-#include "fms_instrument_sequence.h"
 #include "fms_instrument_cd.h"
+#include "fms_instrument_fra.h"
+#include "fms_instrument_swap.h"
